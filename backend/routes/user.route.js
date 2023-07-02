19b8b6228a8a97bfeb42f2c97139e2f66ff61511
ShawnFrost26 <sb262202@gmail.com>
2023-07-02 20:45:30 +0530

    implemented user login

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -16,13 +16,28 @@ userRouter.get('/', async (req, res) => {
 userRouter.post('/register', async (req, res) => {
     try{
         const {name, email, password} = req.body;
-        const user = await User.create({name, email, password})
-        res.status(200).json({message: "user registered sucessfully", user:user})
+        const newUser = await User.create({name, email, password})
+        res.status(200).json({message: "user registered sucessfully", user:newUser})
     }
     catch(error){
         res.status(400).json({message: error})
     }
 })
        
+userRouter.post('/login', async (req, res) => {
+    try{
+        const { email, password} = req.body;
+        const user = await User.findOne({email, password});
+        if(!user){
+            res.status(404).json({message: "user not found"})
+        }
+        else{
+            res.status(201).json({message: "user logged in sucessfully", user:user})
+        }
+    }
+    catch(error){
+        res.status(400).json({message: error})
+    }
+})
 
 module.exports = userRouter;
