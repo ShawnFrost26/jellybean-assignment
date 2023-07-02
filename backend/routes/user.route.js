a6fd9355f2358e3cddde418bf6deb8980e07981b
ShawnFrost26 <sb262202@gmail.com>
2023-07-02 20:29:43 +0530

    implemented user registration

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,17 @@
 const userRouter = require("express").Router();
+const User = require('../models/user.model')
 
 
-
+userRouter.post('/register', async (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+        const user = await User.create({name, email, password})
+        res.status(200).json({message: "user registered sucessfully", user:user})
+    }
+    catch(error){
+        res.status(400).json({message: error})
+    }
+})
+       
 
 module.exports = userRouter;
