315981c818f393191da3bab75690676870da0ab3
ShawnFrost26 <sb262202@gmail.com>
2023-07-03 18:49:54 +0530

    created login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Button, Card, CardContent, TextField, Typography } from "@mui/material";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+  
+    <Card sx={{ p: 4, py: 5, maxWidth: "550px", margin: "50px auto", display: "flex", flexDirection: "column", gap: 4, borderRadius: "15px" }} elevation={10}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Login
+        </Typography>
+      </CardContent>
+        <TextField id="outlined-basic" label="Email" variant="outlined" type={"email"} />
+        <TextField id="outlined-basic" label="Password" variant="outlined" type={"password"}/>
+<Button variant="contained">Login</Button>
+      </Card>
   )
 }
 
