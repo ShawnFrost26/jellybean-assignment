795660881edde18b00c209276b3de2501543dae6
ShawnFrost26 <sb262202@gmail.com>
2023-07-03 23:35:07 +0530

    added update and delete options on clicking MenuVertIcon

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -4,10 +4,9 @@ import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Actions from "./Actions";
 
 export default function BlogCard({ blog }) {
   const { _id, title, content, image, author, createdAt } = blog;
@@ -21,9 +20,7 @@ export default function BlogCard({ blog }) {
           </Avatar>
         }
         action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
+          <Actions/>
         }
         title={title}
         subheader={new Date(createdAt).toDateString()}
