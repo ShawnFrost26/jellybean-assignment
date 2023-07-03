99424525353f8c1e1c8cceaa5b1f5bcb67a010f5
ShawnFrost26 <sb262202@gmail.com>
2023-07-03 18:48:12 +0530

    added container for margiin and padding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import { Container } from "@mui/material";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
+        <Navbar />
+        <Container sx={{p:1, mt:10}}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </Container>
       </BrowserRouter>
     </div>
   );
