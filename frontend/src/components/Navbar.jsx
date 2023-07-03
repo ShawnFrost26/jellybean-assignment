917ce899a6e5a91d0de8a848d7381d1990c2d8b4
ShawnFrost26 <sb262202@gmail.com>
2023-07-03 18:14:43 +0530

    navbar done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,35 +1,107 @@
 import React from "react";
-import { AppBar, Toolbar, Box, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Button,
+  useMediaQuery,
+  IconButton,
+} from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const Navbar = () => {
+  const isLargeScreen = useMediaQuery("(min-width: 1024px)");
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <AppBar>
         <Toolbar sx={{ justifyContent: "space-between" }}>
           <h3>Blogs</h3>
           <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
-            <Button>
-              <Link style={{ color: "#fff", textDecoration: "none" }} to={"/"}>
-                Home
-              </Link>
-            </Button>
-            <Button>
-              <Link
-                style={{ color: "#fff", textDecoration: "none" }}
-                to={"/login"}
-              >
-                Login
-              </Link>
-            </Button>
-            <Button>
-              <Link
-                style={{ color: "#fff", textDecoration: "none" }}
-                to={"/register"}
-              >
-                Register
-              </Link>
-            </Button>
+            {isLargeScreen ? (
+              <>
+                <Button>
+                  <Link style={{ color: "#fff", textDecoration: "none" }}>
+                    Home
+                  </Link>
+                </Button>
+                <Button>
+                  <Link
+                    style={{ color: "#fff", textDecoration: "none" }}
+                    to={"/login"}
+                  >
+                    Login
+                  </Link>
+                </Button>
+                <Button>
+                  <Link
+                    style={{ color: "#fff", textDecoration: "none" }}
+                    to={"/register"}
+                  >
+                    Register
+                  </Link>
+                </Button>
+              </>
+            ) : (
+              <>
+                <IconButton
+                  sx={{ color: "#fff" }}
+                  id="basic-button"
+                  aria-controls={open ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  onClick={handleClick}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                >
+                  <MenuItem onClick={handleClose}>
+                    <Link
+                      style={{ color: "#333", textDecoration: "none" }}
+                      to={"/home"}
+                    >
+                      Home
+                    </Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link
+                      style={{ color: "#333", textDecoration: "none" }}
+                      to={"/login"}
+                    >
+                      Login
+                    </Link>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Link
+                      style={{ color: "#333", textDecoration: "none" }}
+                      to={"/register"}
+                    >
+                      Register
+                    </Link>
+                  </MenuItem>
+                </Menu>
+              </>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
