e5b75b02a361fe273c2bd242b532d6b7546320a0
ShawnFrost26 <sb262202@gmail.com>
2023-07-03 18:59:52 +0530

    adjusted margin

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
   return (
   
     <Card sx={{ p: 4, py: 5, maxWidth: "550px", margin: "50px auto", display: "flex", flexDirection: "column", gap: 4, borderRadius: "15px" }} elevation={10}>
-      <CardContent>
+      <CardContent sx={{ m: 0 }}>
         <Typography gutterBottom variant="h5" component="div">
           Login
         </Typography>
