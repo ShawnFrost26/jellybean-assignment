ac57ef384cfb5ea3e247e861cc1a79879dc3a346
ShawnFrost26 <sb262202@gmail.com>
2023-07-03 17:37:41 +0530

    created navbar and  linked to pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { AppBar, Toolbar, Box, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <AppBar>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <h3>Blogs</h3>
+          <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
+            <Button>
+              <Link style={{ color: "#fff", textDecoration: "none" }} to={"/"}>
+                Home
+              </Link>
+            </Button>
+            <Button>
+              <Link
+                style={{ color: "#fff", textDecoration: "none" }}
+                to={"/login"}
+              >
+                Login
+              </Link>
+            </Button>
+            <Button>
+              <Link
+                style={{ color: "#fff", textDecoration: "none" }}
+                to={"/register"}
+              >
+                Register
+              </Link>
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
