5f7c8d2053a8e7f8f8507a577f946890c8d9ec66
ShawnFrost26 <sb262202@gmail.com>
2023-07-04 08:49:23 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv")
+const cors = require('cors')
 const userRouter = require('./routes/user.route')
 const blogRouter = require('./routes/blog.route')
 
@@ -16,6 +17,7 @@ mongoose.connect(MONGO_URL)
 
 const app = express();
 app.use(express.json());
+app.use(cors({Credential:true}))
 
 app.use("/users", userRouter);
 app.use("/blogs", blogRouter);
