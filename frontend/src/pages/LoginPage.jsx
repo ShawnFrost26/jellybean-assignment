d86eb39af1cceee9ef58d8f371f457930750868f
ShawnFrost26 <sb262202@gmail.com>
2023-07-04 09:23:46 +0530

    minor changes

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import { Button, Card, CardContent, TextField, Typography } from "@mui/material";
+import React from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const LoginPage = () => {
   return (
-  
-    <Card sx={{ p: 4, py: 5, maxWidth: "550px", margin: "50px auto", display: "flex", flexDirection: "column", gap: 4, borderRadius: "15px" }} elevation={10}>
+    <Card
+      sx={{
+        p: 4,
+        py: 5,
+        maxWidth: "550px",
+        margin: "50px auto",
+        display: "flex",
+        flexDirection: "column",
+        gap: 4,
+        borderRadius: "15px",
+      }}
+      elevation={10}
+    >
       <CardContent sx={{ m: 0 }}>
         <Typography gutterBottom variant="h5" component="div">
           Login
         </Typography>
       </CardContent>
-        <TextField id="outlined-basic" label="Email" variant="outlined" type={"email"} />
-        <TextField id="outlined-basic" label="Password" variant="outlined" type={"password"}/>
-<Button variant="contained">Login</Button>
-      </Card>
-  )
-}
+      <TextField id="email" label="Email" variant="outlined" type={"email"} />
+      <TextField
+        id="password"
+        label="Password"
+        variant="outlined"
+        type={"password"}
+      />
+      <Button variant="contained">Login</Button>
+    </Card>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,20 +1,76 @@
-import React from 'react'
-import { Button, Card, CardContent, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const RegisterPage = () => {
-  return (
-    <Card sx={{ p: 4, py: 5, maxWidth: "550px", margin: "50px auto", display: "flex", flexDirection: "column", gap: 3, borderRadius: "15px" }} elevation={10}>
-    <CardContent sx={{ m: 0 }}>
-      <Typography gutterBottom variant="h5" component="div">
-      Register
-      </Typography>
-    </CardContent>
-      <TextField id="outlined-basic" label="Name" variant="outlined" type={"text"} />
-      <TextField id="outlined-basic" label="Email" variant="outlined" type={"email"} />
-      <TextField id="outlined-basic" label="Password" variant="outlined" type={"password"}/>
-<Button variant="contained">Register</Button>
-    </Card>
-  )
-}
+  const [user, setUser] = useState({ name: "", email: "", password: "" });
 
-export default RegisterPage
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <Card
+      sx={{
+        p: 4,
+        py: 5,
+        maxWidth: "550px",
+        margin: "50px auto",
+        display: "flex",
+        flexDirection: "column",
+        gap: 3,
+        borderRadius: "15px",
+      }}
+      elevation={10}
+    >
+      <CardContent sx={{ m: 0 }}>
+        <Typography gutterBottom variant="h5" component="div">
+          Register
+        </Typography>
+      </CardContent>
+      <TextField
+        id="name"
+        label="Name"
+        variant="outlined"
+        type="text"
+        name="name"
+        onChange={handleChange}
+        value={user.name}
+      />
+      <TextField
+        id="email"
+        label="Email"
+        variant="outlined"
+        type="email"
+        name="email"
+        onChange={handleChange}
+        value={user.email}
+      />
+      <TextField
+        id="password"
+        label="Password"
+        variant="outlined"
+        type="password"
+        name="password"
+        onChange={handleChange}
+        value={user.password}
+      />
+      <Button variant="contained" onClick={handleSubmit}>
+        Register
+      </Button>
+    </Card>
+  );
+};
+
+export default RegisterPage;
